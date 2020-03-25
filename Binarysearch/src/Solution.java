19b85037adaa938f157cbcc6860d2374f422520e
rjmane <rachana.j.mane!a>
2020-03-25 14:46:47 +0000

    BS

--- Binarysearch/src/Solution.java
+++ Binarysearch/src/Solution.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+
 public class Solution {
+
+    /**
+     * Return index of occurrence if the number exists in the sorted array
+     */
+    public static int binarySearch(int num, int[] arr) {
+        if (num < arr[0] || num > arr[arr.length-1]) {
+            return -1;
+        }
+        int low = 0;
+        int high = arr.length -1;
+        while (low <= high) {
+            int mid = (low + high )/ 2;
+            if (num == arr[mid]) {
+                return mid;
+            }
+            if (num > arr[mid]) {
+                low = mid + 1;
+            }
+            if (num < arr[mid]) {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
     /**
      * https://www.interviewbit.com/problems/square-root-of-integer/
      */
@@ -22,7 +49,17 @@ public class Solution {
         return (int) high;
     }
 
+    /**
+     * https://www.interviewbit.com/problems/implement-power-function/
+     */
+    public int pow(int x, int n, int d) {
+        return 0;
+    }
+
     public static void main(String[] args) {
-        System.out.println(sqrt(2147483647));
+//        System.out.println(sqrt(2147483647));
+//        int[] arr =  new int[]{1, 2, 3, 6, 6, 9};
+//        System.out.println(binarySearch(1, arr));
+//        System.out.println(nCr(4,2));
     }
 }
